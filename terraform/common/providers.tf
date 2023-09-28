f9a37b35552f80e51b3de2739e9dae71b4f045cd
Osamu-Saiki <123947445+Osamu-Saiki@users.noreply.github.com>
2023-09-28 20:10:37 +0900

    Transfer file from terraform/common/providers.tf to terraform/common/providers.tf

--- terraform/common/providers.tf
+++ terraform/common/providers.tf
@@ -1,1 +1,1 @@
-terraform {  backend "gcs" {    bucket = "mcg-ope-admin-gha-tfstate"    prefix = "mec001-datateam2-my_project_gcp/common"  }  required_providers {    google = ">= 4.0.0"  }}provider "google" {  region       = var.region}
+    prefix = "mec001-datateam2-my_project_gcp/common"
