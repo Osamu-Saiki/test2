5108c2a39dcf1dfcba5d9d72d40ce0ddc9b46e0e
Osamu-Saiki <123947445+Osamu-Saiki@users.noreply.github.com>
2023-09-28 20:10:35 +0900

    Transfer file from terraform/env/template/providers.tf to terraform/env/template/providers.tf

--- terraform/env/template/providers.tf
+++ terraform/env/template/providers.tf
@@ -1,1 +1,1 @@
-terraform {  backend "gcs" {    bucket = "mcg-ope-admin-gha-tfstate"    prefix = "mec001-datateam2-my_project_gcp/__env__"  }  required_providers {    google = ">= 4.0.0"  }}provider "google" {  region       = var.region}
+    prefix = "mec001-datateam2-my_project_gcp/__env__"
