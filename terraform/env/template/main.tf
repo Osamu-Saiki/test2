e4e821d79eec64b45fa0c100ee50d24e36c19f6c
Osamu-Saiki <123947445+Osamu-Saiki@users.noreply.github.com>
2023-09-29 10:04:06 +0900

    Transfer file from terraform/env/template/main.tf to terraform/env/template/main.tf

--- terraform/env/template/main.tf
+++ terraform/env/template/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "common" {
   backend = "gcs"
   config = {
     bucket = "mcg-ope-admin-gha-tfstate"
-    prefix = "mec001-datateam2-my_project_gcp/common"
+    prefix = "mec001-datateam2-myproject/common"
   }
 }
 
