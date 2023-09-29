7f7611c149645a7b99564bc8712237129e5cc238
Osamu-Saiki <123947445+Osamu-Saiki@users.noreply.github.com>
2023-09-29 10:04:07 +0900

    Transfer file from terraform/env/template/providers.tf to terraform/env/template/providers.tf

--- terraform/env/template/providers.tf
+++ terraform/env/template/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket = "mcg-ope-admin-gha-tfstate"
-    prefix = "mec001-datateam2-my_project_gcp/__env__"
+    prefix = "mec001-datateam2-myproject/__env__"
   }
 
   required_providers {
